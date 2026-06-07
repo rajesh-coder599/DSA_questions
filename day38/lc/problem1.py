24c1b5e1e0563b7353314741841ad1ccfae72654
Rajesh Rayal <rayalrajesh6@gmail.com>
2026-06-07 16:02:58 +0000

    DSA question

---
+++ day38/lc/problem1.py
@@ -0,0 +1,30 @@
+# 2196. Create Binary Tree From Descriptions
+
+
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# def createBinaryTree(descriptions):
+#     nodes={}
+#     parent=set()
+#     notparent=set()
+#     for i,j,k in descriptions:
+#         x=TreeNode(i)
+#         y=TreeNode(j)
+#         if i not in nodes:
+#             nodes[i]=x
+#             parent.add(i)
+#             parent
+#         if j not in nodes:
+#             nodes[j]=y
+#             notparent.add(j)
+#     root=nodes[int(parent & notparent)]
+#     visited=set()
+#     for a,b,c in descriptions:
+#         if a not in 
+
+a={1}
+print(int(a))
