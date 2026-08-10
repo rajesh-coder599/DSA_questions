51878d1a2e87b4d26dbd528df233af3a286d1b14
Rajesh Rayal <rayalrajesh6@gmail.com>
2026-08-10 22:09:01 +0530

    dsa questions

---
+++ day67/lc/problem1.py
@@ -0,0 +1,13 @@
+# 1510. Stone Game IV
+
+
+
+# def winnerSquareGame(n):
+#     if type(n**0.5)==int :
+#         return True
+#     x=1
+#     sqr=1
+#     prev=1
+#     while sqr<=n:
+#         prev=x%2
+print(1%2)
